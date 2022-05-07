9513eaad22d96cb5bdb8cd91739e888ca0588070
jaisalsabu <jaisal1010sabu@gmail.com>
2022-05-07 14:47:38 +0100

    #continued

--- CA1.R
+++ CA1.R
@@ -50,6 +50,8 @@ library(viridis)
 
 #-------------------------------------------------------------------------------
 
+#UNI-VARIATE ANALYSIS
+
 #age
 #finding out the maximum and minimum age of in the records
 max_age <- max(heartattack$age)
@@ -109,7 +111,8 @@ max_restingbloodpressure
 min_restingbloodpressure <- min(heartattack$trtbps)
 min_restingbloodpressure
 #plotting the graph for better visualizations
-hist(heartattack$trtbps, main = "Histogram for Resting Blood Pressure", xlab = "Resting Blood pressure")
+hist(heartattack$trtbps, main = "Histogram for Resting Blood Pressure",
+     xlab = "Resting Blood pressure")
 summary(heartattack$trtbps)
 #we use a boxplot for the age variable
 boxplot(heartattack$trtbps)
@@ -130,10 +133,113 @@ min_cholesterol
 #plotting the graph for better visualizations
 hist(heartattack$chol, main = "Histogram for Cholesterol", xlab = "Cholesterol")
 summary(heartattack$chol)
-#we use a boxplot for the age variable
+#we use a boxplot for the cholesterol variable
 boxplot(heartattack$chol)
 #for a clear understanding we will use density plot
 chol <- density(heartattack$chol) # returns the density data
 plot(chol, 
      main="Density plot for Cholesterol",
      xlab = "Cholesterol")
+#analysis - Cholesterol value in most patients is between 200 and 280.
+# Values after 380 are to be considered as outliers.
+
+#-------------------------------------------------------------------------------
+
+#fbs (fasting blood sugar)
+#if fasting blood sugar more than 120md/dl the value is 1(True) or else 0(False)
+fbstrue <- subset( heartattack, fbs == "1")
+print(fbstrue)
+fbsfalse <- subset( heartattack, fbs == "0")
+print(fbsfalse)
+#count of patients with fbs values
+table(heartattack$fbs)
+#we use a bar plot to visualize the data clearly
+fbsplot <- table(heartattack$fbs)
+barplot(fbsplot, main="Fasting Blood Sugar")
+#analysis - a major share of patients have value 0 ~ 85%
+#the blood glucose levels of majority of patients is below 120mg/dl
+#15% patients have a high blood glucose level
+
+#-------------------------------------------------------------------------------
+
+#restecg(resting electrocardiographic results)
+restecg0 <- subset( heartattack, restecg == "0")
+print(restecg0)
+restecg1 <- subset( heartattack, restecg == "1")
+print(restecg1)
+restecg2 <- subset( heartattack, restecg == "2" )
+print(restecg2)
+#count of patients with fbs values
+table(heartattack$fbs)
+#we use a bar plot to visualize the data clearly
+restecgplot <- table(heartattack$restecg)
+barplot(restecgplot, main="resting electrocardiographic results")
+#analysis - the patients with value 2 is very negligible compared to the rest
+#the value 1 and 0 are almost equal with value 1 having a slight head
+#~ 50.2% of patients have ST-T wave abnormality
+#~48.5% of patients have a normal resting electrocardiogram result
+
+#-------------------------------------------------------------------------------
+
+#thalachh (max heart rate)
+max_heartrate <- max(heartattack$thalachh)
+max_hearrate
+min_heartrate <- min(heartattack$thalachh)
+min_heartrate
+#plotting the graph for better visualizations
+hist(heartattack$thalachh, main = "Histogram for maximum heart rate achieved", 
+     xlab = " Max Heart Rate")
+summary(heartattack$thalachh)
+#we use a boxplot for the heartrate variable
+boxplot(heartattack$thalachh)
+#for a clear understanding we will use density plot
+restecgplot <- density(heartattack$thalachh) # returns the density data
+plot(restecgplot, 
+     main="Density plot for Max Heart Rate",
+     xlab = "Heart Rate")
+#analysis - maximum heart rate achieved in most patients is between 145 and 170. 
+
+
+#-------------------------------------------------------------------------------
+
+#exng (Angina induced Exercise)
+exng0 <- subset( heartattack, exng == "0")
+print(exng0)
+exng1 <- subset( heartattack, exng == "1")
+print(exng1)
+#count of patients with Angina induced exercise values
+table(heartattack$exng)
+#we use a bar plot to visualize the data clearly
+exang <- table(heartattack$exng)
+barplot(exang, main="Exercise Induced Angina")
+#analysis - Most of the patients do not have exercise induced angina 
+
+#BI-VARIATE ANALYSIS
+
+#-------------------------------------------------------------------------------
+
+#PART 4 _ DATA TRANSFORMATION
+#we take a look at the column names 
+names(heartattack)
+#we rename a few columns to make the data more understandable
+names(heartattack)[names(heartattack)=="sex"] <- "Gender"
+names(heartattack)[names(heartattack)=="cp"] <- "Chest_pain"
+names(heartattack)[names(heartattack)=="trtbps"] <- "Resting_blood_pressure"
+names(heartattack)[names(heartattack)=="chol"] <- "Cholesterol"
+names(heartattack)[names(heartattack)=="fbs"] <- "Fasting_blood_sugar"
+names(heartattack)[names(heartattack)=="thalachh"] <- "Maximum_heart_rate"
+names(heartattack)[names(heartattack)=="exng"] <- "agina_exercise"
+names(heartattack)[names(heartattack)=="output"] <- "Target"
+#after changing the column names
+names(heartattack)
+#We will not be consider all the variables for our hypothesis , so we have to
+#remove a few variables from our data set
+heart_attack_analysis <- subset(heartattack, select = -c(oldpeak, slp, caa, thall))
+#after removing all the unnecessary columns from our data frame
+heart_attack_analysis
+#names of the columns in new data frame 
+names(heart_attack_analysis)
+
+# PART 5 - HYPOTHESIS TESTING 
+
+# Q1.  
